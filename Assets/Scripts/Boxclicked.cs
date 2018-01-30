8ea418879c381e0785132fc50f90e6e1ef3f8227
yallahha <yallahha@uwo.ca>
2018-01-30 15:13:39 -0500

    This is my first version.

---
+++ Assets/Scripts/Boxclicked.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boxclicked : MonoBehaviour
+{
+
+    public GameObject ground;
+    void Start()
+    {
+    }
+
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        { // if left button pressed...
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.transform.parent.name == ground.name)
+                {
+                    // the object identified by hit.transform was clicked
+                    // do whatever you want
+                    Debug.Log("I was hit");
+                    Vector3 rotate1 = new Vector3(ground.transform.eulerAngles.x, 5, 2);
+                    ground.transform.Rotate(rotate1);
+                    
+                }
+                    //{
+                   // ground.transform.position = new Vector3(ground.transform.eulerAngles.x, 44, ground.transform.eulerAngles.z);
+                    //    Debug.Log(orobject.transform.position);
+                    //}
+                    //else
+                    //{
+                    //  Debug.Log("Nothing");
+                    //}
+                }
+            }
+        }
+    }
+
---
+++ Assets/Scripts/MouseClicked.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseClicked : MonoBehaviour
+{
+    Vector3 orposition;
+    public GameObject orobject;
+    public GameObject ground;
+   // public GameObject newobject;
+
+
+    
+    // Use this for initialization
+    void Start()
+    {
+        orposition = orobject.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray toMouse = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit sss;
+            if (Physics.Raycast(toMouse, out sss))
+            {
+                if (sss.transform.name == orobject.name)
+                {
+                    //{
+                    orobject.transform.position = orposition;
+                    //    Debug.Log(orobject.transform.position);
+                    //}
+                    //else
+                    //{
+                    //  Debug.Log("Nothing");
+                    //}
+                }
+                if (sss.transform.parent.name == ground.name)
+                {
+                    // the object identified by hit.transform was clicked
+                    // do whatever you want 
+                    Debug.Log("I was hit");
+                    Vector3 rotate1 = new Vector3(ground.transform.eulerAngles.x, ground.transform.eulerAngles.y, 2);
+                    ground.transform.Rotate(rotate1);
+
+                }
+            }
+        }
+    }
+}
